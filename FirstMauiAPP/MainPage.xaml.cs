5025c5287212c85d5e5d3cde87fdde138e3c547d
jnavacar <josenavarete@Joses-MacBook-Pro.local>
2025-02-11 12:44:30 +0100

    save and delete notes

--- FirstMauiAPP/MainPage.xaml.cs
+++ FirstMauiAPP/MainPage.xaml.cs
@@ -1,58 +1,30 @@
-﻿namespace Phoneword;
-public partial class MainPage : ContentPage
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace NoteKeeper
 {
-	string? translatedNumber;
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Notes { get; set; } = new ObservableCollection<string>();
 
-	int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-	
-	private void OnTranslate(object sender, EventArgs e)
-	{
-		string enteredNumber = PhoneNumberText.Text;
-		translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+        private void OnSaveNote(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NoteEditor.Text))
+            {
+                Notes.Add(NoteEditor.Text);
+                NoteEditor.Text = "";
+            }
+        }
 
-		if (!string.IsNullOrEmpty(translatedNumber))
-		{
-			CallButton.IsEnabled = true;
-			CallButton.Text = "Call " + translatedNumber;
-		}
-		else
-		{
-			CallButton.IsEnabled = false;
-			CallButton.Text = "Call";
-		}
-	}
-	
-	async void OnCall(object sender, System.EventArgs e)
-	{
-		if (await this.DisplayAlert(
-			    "Dial a Number",
-			    "Would you like to call " + translatedNumber + "?",
-			    "Yes",
-			    "No"))
-		{
-			try
-			{
-				if (PhoneDialer.Default.IsSupported && !string.IsNullOrWhiteSpace(translatedNumber))
-					PhoneDialer.Default.Open(translatedNumber);
-			}
-			catch (ArgumentNullException)
-			{
-				await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
-			}
-			catch (Exception)
-			{
-				// Other error has occurred.
-				await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
-			}
-		}
-
-	}
-
-
+        private void OnDeleteNotes(object sender, EventArgs e)
+        {
+            Notes.Clear();
+        }
+    }
 }
-
